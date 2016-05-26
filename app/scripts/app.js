3006232fdfb64c4276f419e5c1edeeb9b9dcae5d
praneetha sistla <praneethathegreat@gmail.com>
2016-05-26 20:30:00 +0000

    Simple UI without access to camera yet.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,19 @@
-(function() {
+(function () {
     document.addEventListener("deviceready", function () {
-        var app = new kendo.mobile.Application(document.body, { skin: "nova" });
+        window.listview = kendo.observable({
+            addImage: function () {
+                $("#images")
+                    .data("kendoMobileListView")
+                    .prepend(["images/08.jpg"]);
+            }
+        });
+        var app = new kendo.mobile.Application(document.body, {
+            skin: "nova"
+        });
+        $("#images").kendoMobileListView({
+            dataSource: ["images/01.jpg", "images/02.jpg", "images/03.jpg", "images/04.jpg", "images/05.jpg", "images/06.jpg", "images/07.jpg"],
+            template: "<img src='#: data #'>"
+        });
         navigator.splashscreen.hide();
     });
 }());
